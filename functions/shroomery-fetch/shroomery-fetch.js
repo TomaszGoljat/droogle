93c9085c95d0bedf78f28488a03f66627272ae43
goljatt <tgoljat@protonmail.com>
2023-09-14 11:49:48 +0200

    modified:   .gitignore 	modified:   functions/shroomery-fetch/shroomery-fetch.js 	deleted:    netlify/functions/shroomery.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/components/Search.js

--- functions/shroomery-fetch/shroomery-fetch.js
+++ functions/shroomery-fetch/shroomery-fetch.js
@@ -1,17 +1,34 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+const url = "https://www.shroomery.org/forums/dosearch.php?where=body&tosearch=both&how=all&words=grow+box"
+
+//const result = axios.get(url)
+async function scrap() {
+  const results = await axios.get(url)
 }
 
-module.exports = { handler }
+
+
+exports.handler = async function (event, context) {
+  const response = await axios.get(url);
+  //const searchResults = response.data;
+  // Let's load the html from response:
+  const $ = cheerio.load(response.data)
+  // Selecting actual results:
+  const resultsList = $(".pp")
+  // Creating table to send to front end
+  const resultsTable = []
+  // Adding results to the array:
+  resultsList.each((idx, el) =>
+  {
+    resultsTable.push([$(el).text(), $(el).attr("href")])
+  })
+  //console.log(`Here's the data from website: ${searchResults}`)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(resultsTable)
+  };
+};
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 /*
 * Search Component display searchbox and search buttons.
 * Takes the input and when clicked on search button - sends it to chosen search component to fetch and display.
 */
 
+
+
 export default function Search() {
+
+    const url = "http://localhost:8888/.netlify/functions/shroomery-fetch/shroomery-fetch.js"
+
+    /*
+    const fetchResult = async function simpleFetch() {
+        const response = await axios.get(url)
+        return response.data.body
+    }
+    
+This one worked
+    const GetResults = async () => {
+        let { data } = await axios.get(url);
+        console.log(data)
+    }
+*/
+
+
+    const [results, setResults] = React.useState([]);
+    const [error, setError] = React.useState(false);
+    const [state, setState] = React.useState('');
+
+    function fetchResults() {
+        setState('loading')
+        axios
+            .get(url)
+            .then((res) => {
+                console.log(res)
+                setState('success')
+                setResults(res.data)
+            })
+            .catch((err) => {
+                console.error('Error:',err)
+                setState('error')
+                setError(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchResults();
+    }, [])
+
+    if(state === 'error')
+        return (
+    <h1>{error.toString()}</h1>
+    );
+/*
     return (
+        <div>
+            {results.map(el => {
+                return <p><a href={el[1]}>{el[0]}</a></p>
+            })}
+        </div>
+    )
+*/
+    
+
+        return (
         <div className="search">
         <div className="search-box">
             <input type="text" className="search-input" />
@@ -20,4 +79,5 @@ export default function Search() {
         </div>
         </div>
     )
+    
 }
