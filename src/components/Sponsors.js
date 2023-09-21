c77878655d3ebd8c1da5ac1ed0b7b67077ba1686
goljatt <tgoljat@protonmail.com>
2023-09-21 14:35:53 +0200

    modified:   src/App.css 	modified:   src/components/Sponsors.js

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -2,7 +2,28 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 
 export default function Sponsors() {
+
+    const Sponsor = (props) => {
+        return (
+            <a href={props.url}>
+                <div className="sponsor--div">
+                    <p className="sponsor--title">{props.title}</p>
+                    <p className="sponsor--desc">{props.description ? props.description : "Supporter"}</p>
+                </div>
+            </a>
+        )
+    }
+
     return (
-        <Marquee>This month supporters: Zamnesia, BigPharma, YoMama, Elon Musk, Joe Rogan, Pan Tadeusz...</Marquee>
+        <div className="sponsors">
+            <div>Supporters: </div>
+            <Marquee pauseOnHover={true} autoFill={true} className="sponsors--marquee">
+                <Sponsor title="Zamnesia" url="zamnesia.com" description="Shop at Zamnesia.com" />
+                <Sponsor title="Azarius" url="zamnesia.com" description="Shop at Azarius.com" />
+                <Sponsor title="Elon Musk" url="zamnesia.com" description="Buy Tesla" />
+                <Sponsor title="Pan Tadeusz" url="zamnesia.com" description="" />
+                <Sponsor title="Bisz" url="zamnesia.com" description="Deus Ex Machina" />
+            </Marquee>
+        </div>
     )
 }
