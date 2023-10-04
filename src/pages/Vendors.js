d44cb674fe01b4daf7b77c0bc8126041c3b7e4c8
goljatt <tgoljat@protonmail.com>
2023-10-04 12:28:06 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/data/vendors.json 	modified:   src/pages/Vendors.js

--- src/pages/Vendors.js
+++ src/pages/Vendors.js
@@ -1,13 +1,20 @@
 import axios from "axios"
 import React from "react"
 import { useEffect } from "react"
+import { useSelect } from "downshift"
 
 
 
 export default function Vendors() {
 
     const [vendorArr, setVendorArr] = React.useState(require("../data/vendors.json"))
+    const [country, setCountry] = React.useState(null)
+    const [filteredArr, setFilteredArr] = React.useState(vendorArr)
 
+    function handleSelectedItemChange({selectedItem}) {
+        setCountry(selectedItem)
+        setFilteredArr(filterByCountry(selectedItem))
+      }
     const SupporterTag = (props) => {
         let tag = ""
         switch (props.level) {
@@ -31,6 +38,57 @@ export default function Vendors() {
         return tag
     }
 
+    /*  const countries = List of all of the countries
+    *   1. Create an array of country property from each object (map).
+    *   2. Filter through the array and get rid of duplicates (filter).
+    *   3. Profit!
+    */
+   const countries = vendorArr.map(v => v.country).filter((item, i, ar) => ar.indexOf(item) === i)
+   const items = countries;
+
+
+    function filterByCountry(country) {
+        console.log(`Inside filterByCountry country is: ${country}`)
+        return vendorArr.filter(vendor => vendor.country === country)
+    }
+
+   function DropdownSelect({selectedItem, handleSelectedItemChange}) {
+        const {
+          isOpen,
+          getToggleButtonProps,
+          getLabelProps,
+          getMenuProps,
+          highlightedIndex,
+          getItemProps,
+        } = useSelect({
+          items,
+          selectedItem,
+          onSelectedItemChange: handleSelectedItemChange,
+        })
+        return (
+          <div>
+            <label {...getLabelProps()}>Filter by country:</label>
+            <div className="vendors--selectedCountry" {...getToggleButtonProps()}>
+              {selectedItem ?? 'Choose Country'}
+            </div>
+            <ul {...getMenuProps()} className="vendors--countryList">
+              {isOpen &&
+                items.map((item, index) => (
+                  <li
+                    style={
+                      highlightedIndex === index ? {backgroundColor: 'blue'} : {}
+                    }
+                    key={`${item}${index}`}
+                    {...getItemProps({item, index})}
+                  >
+                    {item}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        )
+      }
+
    const SingleVendor = (props) => {
     return (
     <div className="singleVendor--div">
@@ -42,10 +100,10 @@ export default function Vendors() {
         </div>
         <div className="singleVendor--descBox">{props.desc}</div>
         <div className="singleVendor--tagBox">
-            {props.tags.map(tag => <div className="singleVendor--tag">{tag}</div>)}
+            {props.tags.map((tag, index) => <div className="singleVendor--tag" key={`${tag}${index}`}>{tag}</div>)}
         </div>
         <div className="singleVendor--couponBox">
-            {props.coupon === "" ? "" : <span>{props.coupon}</span>}
+            {props.coupon === 0 ? "" : <span>{props.coupon}</span>}
         </div>
     </div>
     )
@@ -54,16 +112,22 @@ export default function Vendors() {
 
     return (
         <div className="vendors--div">
+            <div className="vendors--filters">
+            <DropdownSelect selectedItem={country} handleSelectedItemChange={handleSelectedItemChange} />
+
+            </div>
             <h1>Vendors List</h1>
-            {console.log(vendorArr[0].name)}
+    {console.log(country)}
             <p>Here you will find list of shops that you can filter through by:</p>
-            {vendorArr.map(v => {return (
-            <SingleVendor name={v.name} 
+            {filteredArr.map((v, index) => {return (
+            <SingleVendor 
+            name={v.name} 
             sponsor={v.sponsor}
             desc = {v.description}
             country = {v.country}
             tags = {v.tags.split(", ")}
             coupon = {v.coupon}
+            key = {`${v.name}${index}`}
             />)})}
         </div>
     )
