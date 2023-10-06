77228b54517c5816555a5a153090e8c31925127c
goljatt <tgoljat@protonmail.com>
2023-10-06 11:52:23 +0200

    modified:   src/index.js 	modified:   src/pages/Vendors.js

--- src/pages/Vendors.js
+++ src/pages/Vendors.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import React from "react"
 import { useEffect } from "react"
-import { useSelect } from "downshift"
+import { useSelect, useCombobox } from "downshift"
 
 
 
@@ -9,19 +9,17 @@ export default function Vendors() {
 
     const [vendorArr, setVendorArr] = React.useState(require("../data/vendors.json"))
     const [country, setCountry] = React.useState(null)
+    const [selectedTags, setSelectedTags] = React.useState([])
     const [filteredArr, setFilteredArr] = React.useState(vendorArr)
 
-    function handleSelectedItemChange({selectedItem}) {
-        setCountry(selectedItem)
-        setFilteredArr(filterByCountry(selectedItem))
-      }
+
     const SupporterTag = (props) => {
         let tag = ""
         switch (props.level) {
             case 0:
             default:
                 tag = ""
-            break;
+                break;
             case 1:
                 tag = <div className="supporter-bronze">bronze supporter</div>
                 break;
@@ -38,97 +36,235 @@ export default function Vendors() {
         return tag
     }
 
+    /*
+    * ..:: COUNTRY FILTER START ::..
+    */
+
     /*  const countries = List of all of the countries
     *   1. Create an array of country property from each object (map).
     *   2. Filter through the array and get rid of duplicates (filter).
     *   3. Profit!
     */
-   const countries = vendorArr.map(v => v.country).filter((item, i, ar) => ar.indexOf(item) === i)
-   const items = countries;
-
+    const countries = vendorArr.map(v => v.country).filter((item, i, ar) => ar.indexOf(item) === i)
 
     function filterByCountry(country) {
         console.log(`Inside filterByCountry country is: ${country}`)
         return vendorArr.filter(vendor => vendor.country === country)
     }
 
-   function DropdownSelect({selectedItem, handleSelectedItemChange}) {
+    function handleSelectedCountryChange({ selectedItem }) {
+        setCountry(selectedItem)
+        setFilteredArr(filterByCountry(selectedItem))
+    }
+
+    function CountrySelect({ selectedItem }) {
+        const {
+            isOpen,
+            getToggleButtonProps,
+            getLabelProps,
+            getMenuProps,
+            highlightedIndex,
+            getItemProps,
+        } = useSelect({
+            items: countries,
+            selectedItem: country,
+            onSelectedItemChange: handleSelectedCountryChange,
+        })
+        return (
+            <div>
+                <label {...getLabelProps()}>Filter by country:</label>
+                <div className="vendors--selectedCountry" {...getToggleButtonProps()}>
+                    {country ?? 'Choose Country'}
+                </div>
+                <ul {...getMenuProps()} className="vendors--countryList">
+                    {isOpen &&
+                        countries.map((item, index) => (
+                            <li
+                                style={
+                                    highlightedIndex === index ? { backgroundColor: 'blue' } : {}
+                                }
+                                key={`${item}${index}`}
+                                {...getItemProps({ item, index })}
+                            >
+                                {item}
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        )
+    }
+
+    /*
+    * ..:: COUNTRY FILTER END ::..
+    */
+
+    /*
+    * ..:: TAGS FILTER START ::..
+    */
+
+    // Getting all of the tags:
+
+
+    const tags = vendorArr.map(v => [...v.tags.split(", ")]).flat(1).filter((item, i, ar) => ar.indexOf(item) === i)
+
+    const comboboxStyles = {}
+    const menuStyles = {}
+    
+
+    function handleSelectedTagsChange() {
+
+    }
+    function DropdownCombobox( {selectedItems} ) {
+        const [inputItems, setInputItems] = React.useState(tags)
         const {
           isOpen,
           getToggleButtonProps,
           getLabelProps,
           getMenuProps,
+          getInputProps,
           highlightedIndex,
           getItemProps,
-        } = useSelect({
-          items,
-          selectedItem,
-          onSelectedItemChange: handleSelectedItemChange,
+        } = useCombobox({
+          items: inputItems,
+          onSelectedItemChange: ({selectedItem}) => {
+            if (!selectedItem) {
+              return
+            }
+            const index = selectedItems.indexOf(selectedItem)
+            if (index > 0) {
+              setSelectedTags([
+                ...selectedItems.slice(0, index),
+                ...selectedItems.slice(index + 1),
+              ])
+            } else if (index === 0) {
+              setSelectedTags([...selectedItems.slice(1)])
+            } else {
+              setSelectedTags([...selectedItems, selectedItem])
+            }
+          },
+          selectedItem: null,
+          stateReducer: (state, actionAndChanges) => {
+            const {changes, type} = actionAndChanges
+            switch (type) {
+              case useCombobox.stateChangeTypes.InputKeyDownEnter:
+              case useCombobox.stateChangeTypes.ItemClick:
+                return {
+                  ...changes,
+                  isOpen: true, // keep menu open after selection.
+                  highlightedIndex: state.highlightedIndex,
+                  inputValue: '', // don't add the item string as input value at selection.
+                }
+              case useCombobox.stateChangeTypes.InputBlur:
+                return {
+                  ...changes,
+                  isOpen: true,
+                  inputValue: '', // don't add the item string as input value at selection.
+                }
+              default:
+                return {
+                    ...changes,
+                    isOpen: true
+                }
+            }
+          },
+          onInputValueChange: ({inputValue}) => {
+            setInputItems(
+              tags.filter((item) =>
+                item.toLowerCase().startsWith(inputValue.toLowerCase()),
+              ),
+            )
+          },
         })
+        const placeholderText = selectedItems.length
+          ? `${selectedItems.length} tags selected`
+          : 'elements'
         return (
           <div>
-            <label {...getLabelProps()}>Filter by country:</label>
-            <div className="vendors--selectedCountry" {...getToggleButtonProps()}>
-              {selectedItem ?? 'Choose Country'}
+            <label {...getLabelProps()}>Choose tags:</label>
+            <div style={comboboxStyles}>
+              <input placeholder={placeholderText} {...getInputProps()} />
+              <button
+                type="button"
+                {...getToggleButtonProps()}
+                aria-label="toggle menu"
+              >
+                &#8595; a
+              </button>
             </div>
-            <ul {...getMenuProps()} className="vendors--countryList">
+            <div {...getMenuProps()} style={menuStyles}>
               {isOpen &&
-                items.map((item, index) => (
-                  <li
+                inputItems.map((item, index) => (
+                  <div
                     style={
                       highlightedIndex === index ? {backgroundColor: 'blue'} : {}
                     }
                     key={`${item}${index}`}
-                    {...getItemProps({item, index})}
+                    {...getItemProps({
+                      item,
+                      index,
+                      'aria-selected': selectedItems.includes(item),
+                    })}
                   >
+                    <input
+                      type="checkbox"
+                      checked={selectedItems.includes(item)}
+                      value={item}
+                      onChange={() => null}
+                    />
+                    <span />
                     {item}
-                  </li>
+                  </div>
                 ))}
-            </ul>
+            </div>
           </div>
         )
       }
+    /*
+    * ..:: TAGS FILTER END ::.. 
+    */
 
-   const SingleVendor = (props) => {
-    return (
-    <div className="singleVendor--div">
-        <div className="singleVendor--nameBox">
-            <span className="singleVendor--name">{props.name}</span>
-            <span className="singleVendor--sponsor">
-                <SupporterTag level={props.sponsor} />
-            </span>
-        </div>
-        <div className="singleVendor--descBox">{props.desc}</div>
-        <div className="singleVendor--tagBox">
-            {props.tags.map((tag, index) => <div className="singleVendor--tag" key={`${tag}${index}`}>{tag}</div>)}
-        </div>
-        <div className="singleVendor--couponBox">
-            {props.coupon === 0 ? "" : <span>{props.coupon}</span>}
-        </div>
-    </div>
-    )
-   } 
+
+    const SingleVendor = (props) => {
+        return (
+            <div className="singleVendor--div">
+                <div className="singleVendor--nameBox">
+                    <span className="singleVendor--name">{props.name}</span>
+                    <span className="singleVendor--sponsor">
+                        <SupporterTag level={props.sponsor} />
+                    </span>
+                </div>
+                <div className="singleVendor--descBox">{props.desc}</div>
+                <div className="singleVendor--tagBox">
+                    {props.tags.map((tag, index) => <div className="singleVendor--tag" key={`${tag}${index}`}>{tag}</div>)}
+                </div>
+                <div className="singleVendor--couponBox">
+                    {props.coupon === 0 ? "" : <span>{props.coupon}</span>}
+                </div>
+            </div>
+        )
+    }
 
 
     return (
         <div className="vendors--div">
             <div className="vendors--filters">
-            <DropdownSelect selectedItem={country} handleSelectedItemChange={handleSelectedItemChange} />
-
+                <CountrySelect />
+                <DropdownCombobox selectedItems={selectedTags}/>
             </div>
             <h1>Vendors List</h1>
-    {console.log(country)}
             <p>Here you will find list of shops that you can filter through by:</p>
-            {filteredArr.map((v, index) => {return (
-            <SingleVendor 
-            name={v.name} 
-            sponsor={v.sponsor}
-            desc = {v.description}
-            country = {v.country}
-            tags = {v.tags.split(", ")}
-            coupon = {v.coupon}
-            key = {`${v.name}${index}`}
-            />)})}
+            {filteredArr.map((v, index) => {
+                return (
+                    <SingleVendor
+                        name={v.name}
+                        sponsor={v.sponsor}
+                        desc={v.description}
+                        country={v.country}
+                        tags={v.tags.split(", ")}
+                        coupon={v.coupon}
+                        key={`${v.name}${index}`}
+                    />)
+            })}
         </div>
     )
 }
